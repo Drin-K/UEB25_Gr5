83f82f23c92dbab59ed04115fe5bf4f85d4e3e3a
albenaveseli <161362598+albenaveseli@users.noreply.github.com>
2025-04-03 11:35:51 +0200

    funksioni per gjenerimin e menuse

--- html/index.php
+++ html/index.php
@@ -1,6 +1,7 @@
 <?php
 // Definimi i konstantave dhe variablave
 const SITE_NAME = "ILLYRIAN Gym";
+//vargu asociativ
 $menu_items = [
     "index.php" => "Home",
     "services.php" => "Services",
@@ -10,7 +11,16 @@ $menu_items = [
     "workouts.php" => "Workouts",
     "review.php" => "Review"
 ];
-
+// Funksion për të gjeneruar menunë
+function generateMenu($items, $activePage) {
+    $menuHtml = "";
+    foreach ($items as $link => $title) {
+        $activeClass = ($link === $activePage) ? 'style="color:aquamarine; border-bottom: 3px solid var(--main-color);"' : '';
+        $menuHtml .= "<li><a href='$link' $activeClass>$title</a></li>";
+    }
+    return $menuHtml;
+}
+?>
 
 
 <!DOCTYPE html>
