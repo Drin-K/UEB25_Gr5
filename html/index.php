6d7ec200e53432bacc88f4d0efd00454194a851f
albenaveseli <161362598+albenaveseli@users.noreply.github.com>
2025-04-03 11:36:25 +0200

    integrimi i php ne shfaqjen e titullit te faqes

--- html/index.php
+++ html/index.php
@@ -30,7 +30,7 @@ function generateMenu($items, $activePage) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
-    <title>ILLYRIAN Gym</title>
+    <title><?php rcho SITE_NAME?></title>
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script> 
 </head>
 
