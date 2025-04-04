a95a1e54f597966dfb49f95b9eb308405de268fc
albenaveseli <albena.veseli@student.uni-pr.edu>
2025-04-04 20:35:36 +0200

    gjenerimi i menuse permes php

--- php/diet.php
+++ php/diet.php
@@ -1,3 +1,29 @@
+<?php
+// Definimi i konstantave dhe variablave
+const SITE_NAME = "ILLYRIAN Gym";
+//vargu asociativ
+$menu_items = [
+    "index.php" => "Home",
+    "services.php" => "Services",
+    "diet.php" => "Diet",
+    "about.php" => "About Us",
+    "plans.php" => "Pricing",
+    "workouts.php" => "Workouts",
+    "review.php" => "Review"
+];
+// Funksion për të gjeneruar menunë
+function generateMenu($items, $activePage) {
+    $menuHtml = "";
+    foreach ($items as $link => $title) {
+        $activeClass = ($link === $activePage) ? 'style="color:aquamarine; border-bottom: 3px solid var(--main-color);"' : '';
+        $menuHtml .= "<li><a href='$link' $activeClass>$title</a></li>";
+    }
+    return $menuHtml;
+}
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -63,19 +89,11 @@ td {
 
 <body>
     <header>
-        <a href="index.html" class="logo">ILLYRIAN <span>Gym</span></a>
-
+        <a href="index.php" class="logo"><?php echo SITE_NAME;?></a>
         <div class="bx bx-menu" id="menu-icon"></div>
-
         <ul class="navbar">
-            <li><a href="index.html">Home</a></li>
-            <li><a href="services.html">Services</a></li>
-            <li><a href="diet.html"style="color:aquamarine;  border-bottom: 3px solid var(--main-color);">Diet</a></li>
-            <li><a href="about.html">About Us</a></li>
-            <li><a href="plans.html">Pricing</a></li>
-            <li><a href="Workouts.html">Workouts</a></li>
-            <li><a href="review.html">Review</a></li>
-        </ul>
+            <?php echo generateMenu($menu_items, basename($_SERVER['PHP_SELF']));?>
+      </ul>
         <div class="top-btn">
             <a href="joinus.html" class="nav-btn">Join Us</a>
         </div>
