87771515cebee1b1af051fe8dec3cf50f3a621fa
Drin-K <161349561+Drin-K@users.noreply.github.com>
2025-04-08 14:13:07 +0200

    Krijimi i headers.php
    Reduktim i kodit

--- php/about.php
+++ php/about.php
@@ -1,23 +1,6 @@
 <?php
-//vargu asociativ
-$menu_items = [
-    "index.php" => "Home",
-    "services.php" => "Services",
-    "diet.php" => "Diet",
-    "about.php" => "About Us",
-    "plans.php" => "Pricing",
-    "workouts.php" => "Workouts",
-    "review.php" => "Review"
-];
-// Funksion për të gjeneruar menunë
-function generateMenu($items, $activePage) {
-    $menuHtml = "";
-    foreach ($items as $link => $title) {
-        $activeClass = ($link === $activePage) ? 'style="color:aquamarine; border-bottom: 3px solid var(--main-color);"' : '';
-        $menuHtml .= "<li><a href='$link' $activeClass>$title</a></li>";
-    }
-    return $menuHtml;
-}?>
+include 'headers.php';
+?>
 
 <!DOCTYPE html>
 <html lang="en">
--- php/plans.php
+++ php/plans.php
@@ -1,26 +1,5 @@
 <?php
-// Vargu asociativ
-$menu_items = [
-    "index.php" => "Home",
-    "services.php" => "Services",
-    "diet.php" => "Diet",
-    "about.php" => "About Us",
-    "plans.php" => "Pricing",
-    "workouts.php" => "Workouts",
-    "review.php" => "Review"
-];
-
-// Funksion për të gjeneruar menunë
-function generateMenu($items, $activePage) {
-    $menuHtml = "";
-    foreach ($items as $link => $title) {
-        $activeClass = ($link === $activePage) ? 'style="color:aquamarine; border-bottom: 3px solid var(--main-color);"' : '';
-        $menuHtml .= "<li><a href='$link' $activeClass>$title</a></li>";
-    }
-    return $menuHtml;
-}
-
-
+include 'headers.php';
 // Vargu asociativ për planet dhe çmimet
 $plans = [
     "BASIC" => 30,
