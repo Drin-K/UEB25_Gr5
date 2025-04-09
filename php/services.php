f7f58e63e25c352e3f012af5f8cc5da7746f2f1f
albenaveseli <albena.veseli@student.uni-pr.edu>
2025-04-09 15:59:40 +0200

    rregullim i nje gabimi sintaksor

--- php/services.php
+++ php/services.php
@@ -1,5 +1,28 @@
 <?php
+<<<<<<< Updated upstream
 include 'headers.php';
+=======
+// Definimi i konstantave dhe variablave
+define("SITE_NAME", "ILLYRIAN Gym");
+$menu_items = [
+"index.php" => "Home",
+"services.php" => "Services",
+"diet.php" => "Diet",
+"about.php" => "About Us",
+"plans.php" => "Pricing",
+"workouts.php" => "Workouts",
+"review.php" => "Review"
+];
+// Funksion për të gjeneruar menunë
+function generateMenu($items, $activePage) {
+$menuHtml = "";
+foreach ($items as $link => $title) {
+$activeClass = ($link === $activePage) ? 'style="color:aquamarine; border-bottom: 3px solid var(--main-color);"' : '';
+$menuHtml .= "<li><a href='$link' $activeClass>$title</a></li>";
+}
+return $menuHtml;
+}
+>>>>>>> Stashed changes
 // Klasa bazë për shërbimet
 class Service {
 protected string $image;
