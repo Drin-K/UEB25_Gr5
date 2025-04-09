ab2c1faf4494a45f9b8e01285834f43ea74d627c
albenaveseli <albena.veseli@student.uni-pr.edu>
2025-04-09 16:06:38 +0200

    reduktim i kodit

--- php/services.php
+++ php/services.php
@@ -1,25 +1,6 @@
 <?php
 include 'headers.php';
-// Definimi i konstantave dhe variablave
-define("SITE_NAME", "ILLYRIAN Gym");
-$menu_items = [
-"index.php" => "Home",
-"services.php" => "Services",
-"diet.php" => "Diet",
-"about.php" => "About Us",
-"plans.php" => "Pricing",
-"workouts.php" => "Workouts",
-"review.php" => "Review"
-];
-// Funksion për të gjeneruar menunë
-function generateMenu($items, $activePage) {
-$menuHtml = "";
-foreach ($items as $link => $title) {
-$activeClass = ($link === $activePage) ? 'style="color:aquamarine; border-bottom: 3px solid var(--main-color);"' : '';
-$menuHtml .= "<li><a href='$link' $activeClass>$title</a></li>";
-}
-return $menuHtml;
-}
+
 // Klasa bazë për shërbimet
 class Service {
 protected string $image;
