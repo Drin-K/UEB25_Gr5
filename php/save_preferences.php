90e8f7cfb236dad3d3b4fc62c7120f24823ced62
Drin-K <161349561+Drin-K@users.noreply.github.com>
2025-05-20 16:10:01 +0200

    Update save_preferences.php

--- php/save_preferences.php
+++ php/save_preferences.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include("db.php");
+require_once("db.php");
 
 if (!isset($_SESSION['user_id']) || $_SERVER['REQUEST_METHOD'] !== 'POST') {
     header("Location: login.php");
